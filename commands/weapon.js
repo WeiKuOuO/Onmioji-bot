3140d358caf3fe1a61b65b1a73f5b53555cca6bd
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-01-26 23:09:17 +0800

    .

--- commands/weapon.js
+++ commands/weapon.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-    if(args[0] == list){
+    if(args[1] == list){
         message.channel.send('hi ghostblade1')
     }
-    if(args[0] == ghostblade1){
+    if(args[1] == ghostblade1){
         message.channel.send('https://i.imgur.com/13mrLGm.png')
     }
 
