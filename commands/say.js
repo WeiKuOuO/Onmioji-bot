7fe43141192b247770dd248d8fa17b7a88bafd35
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-01-26 23:16:27 +0800

    .

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 var discord = require('discord.js');
 
 module.exports.run = (bot, message, args, tools) => {
-    if (args[1] == green){
+    if (args[1] == "green"){
         const text = args.slice(1).join(" ")
         message.delete().catch(O_o=>{});
         message.channel.send({embed: {
