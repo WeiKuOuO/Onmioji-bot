019f1064936a0b09ed91ea4a5351f45bea465214
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-01-26 23:25:16 +0800

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,14 +1,18 @@
 var discord = require('discord.js');
 
-module.exports.run = (bot, message, args, tools) => {
-    if (args[1] == "green"){
+module.exports.run = (bot, message, args) => {
+    if(args[0] == 'green', function(err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
         const text = args.slice(1).join(" ")
         message.delete().catch(O_o=>{});
         message.channel.send({embed: {
             description: (text),
             color: 0x00FF04,
          }})
-    }
+    });
 }
 module.exports.help = {
   name: "say",
