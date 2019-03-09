81abe467c9bd0ce584a777bcffcfb12ea35f2aff
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-03-09 15:49:01 +0800

    Update weapon.js

--- commands/weapon.js
+++ commands/weapon.js
@@ -1,9 +1,18 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+    const weapon_list = new Discord.RichEmbed()
+      .setAuthor(bot.user.username)
+      .setTitle("武器列表")
+      .setColor("RANDOM")
+      .setDescription("")
+      .addField("武器名稱",`\`\`\`diff\n計算中...\`\`\``, true)
+      .addField("代碼比對",`\`\`\`xl\n計算中...\`\`\``, true)
+
+    message.channel.send(weapon_list)
 
     if(args[0] == "list"){
-        message.channel.send("test")
+        message.channel.send(weapon_list)
     }
 
     if(args[0] == "ghostblade1"){
