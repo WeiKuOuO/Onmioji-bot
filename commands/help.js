eaa6837b901e3b9672b7e575d4e2b6f8c6c434fa
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2020-01-17 22:22:51 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,9 +8,9 @@ module.exports.run = async (bot, message, args) => {
         .setAuthor(bot.user.username, bot.user.avatarURL)
         .setFooter(bot.user.username, bot.user.avatarURL)
         .setTitle("指令列表")
-        .addField('• 指令', "```py\n1# pixel/help\n2# pixel/ascii\n3# pixel/purge\n```",true )
+        .addField('• 指令', "```py\n1# .help\n2# .ascii\n3# .purge\n```",true )
         .addField('• 說明', "```fix\n開啟此列表\n藝術文字 (Admin)\n大量刪除訊息 (Admin)\n```",true )
-        .addField('• 建議', "(Admin) 代表需要Admin權限\n如果有任何有關於指令的建議，像是想要新增指令或是功能\n還歡迎聯絡 ***微苦#3402***\n我會很樂意為你服務的")
+        .addField('• 建議', "(Admin) 代表需要Admin權限\n如果有任何有關於指令的建議，像是想要新增指令或是功能\n還歡迎聯絡 ***AiverAiva#3402***\n我會很樂意為你服務的")
         .setTimestamp(new Date())
         .setFooter(`${message.author.tag}`);
 
