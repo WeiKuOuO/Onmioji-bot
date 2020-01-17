d8b522c519d427e289969e6f9cd6f3da8991dae0
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2020-01-17 22:34:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ bot.commands = new Discord.Collection();
 
 let index = 0;
 
+bot.on("message", async message => {
   //command handler
 	if (message.content.toLowerCase().indexOf(prefix) !== 0) return
     	const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -27,7 +28,8 @@ let index = 0;
 		message.reply(`未知指令! 請輸入 **${prefix}help** 查看指令列表`)
   	}
   	if(message.content.indexOf(prefix) !== 0) return;
-
+}
+       
 bot.on("ready", async () => {
   console.log(`${bot.user.username}成功啟動了!^w^, [ ${bot.guilds.size} | ${bot.channels.size} | ${bot.users.size} ]`);
   bot.channels.filter(c => c.name=="陰陽師機器人資訊").forEach(c => c.bulkDelete("50"))
